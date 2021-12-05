4fa6232640c8eee4c0a2de6981227113c7e18700
ilyas <ilyasers@gmail.com>
2021-12-05 21:34:52 +0300

    feat: add renderers for feeds and posts

--- src/view.js
+++ src/view.js
@@ -10,15 +10,13 @@ const renderForm = ({
   addButton.disabled = form.state === 'validating';
   feedback.textContent = '';
 
-  const [error] = form.errors;
-
   switch (form.state) {
     case 'valid':
       url.classList.remove('is-invalid');
       break;
     case 'invalid':
       url.classList.add('is-invalid');
-      feedback.textContent = i18n.t(`errorsMessages.${error}`);
+      feedback.textContent = i18n.t(`errorsMessages.${form.errors[0]}`);
       break;
     case 'validating':
       break;
@@ -27,8 +25,49 @@ const renderForm = ({
   }
 };
 
+const renderFeeds = ({ state, i18n, elements }) => {
+  const { feeds } = elements;
+
+  feeds.innerHTML = '';
+
+  if (!state.feeds.length) return;
+
+  const feedsItems = state.feeds.map((item) => (
+    `<div>
+      <a href=${item.url} target="_blank">${item.title}</a>
+      <p>${item.description}</p>
+    </div>`
+  ));
+
+  feeds.innerHTML = `
+    <h2>${i18n.t('feeds.title')}</h2>
+    ${feedsItems.join('\n')}
+  `;
+};
+
+const renderPosts = ({ state, i18n, elements }) => {
+  const { posts } = elements;
+
+  posts.innerHTML = '';
+
+  if (!state.posts.length) return;
+
+  const sortedPostItems = state.posts.sort((a, b) => b.pubDate - a.pubDate);
+
+  const postsItems = sortedPostItems.map((item) => (
+    `<div>
+      <a href=${item.url} target="_blank">${item.title}</a>
+    </div>`
+  ));
+
+  posts.innerHTML = `
+    <h2>${i18n.t('posts.title')}</h2>
+    ${postsItems.join('\n')}
+  `;
+};
+
 const render = ({
-  path = null, state, i18n, elements,
+  path = '', state, i18n, elements,
 }) => {
   const { form } = state;
 
@@ -39,6 +78,18 @@ const render = ({
       elements,
     });
   }
+
+  renderFeeds({
+    state,
+    i18n,
+    elements,
+  });
+
+  renderPosts({
+    state,
+    i18n,
+    elements,
+  });
 };
 
 export default render;
